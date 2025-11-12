28db37979a3b44a69a4dfc6941808dc57698f0fb
Morteza <msetoodeh13@gmail.com>
2025-11-12 14:34:42 +0330

    Services Added

--- my_web/style.js
+++ my_web/style.js
@@ -1,15 +1,13 @@
-  
-   const scrollBtn = document.querySelector('#homebtn');
-    const section2 = document.querySelector('#about');
+const scrollBtn = document.querySelector("#homebtn");
+const section2 = document.querySelector("#forbtn");
 
-    window.addEventListener('scroll', () =>{
-      const rect = section2.getBoundingClientRect();
-      const windowHeight = window.innerHeight;
+window.addEventListener("scroll", () => {
+  const rect = section2.getBoundingClientRect();
+  const windowHeight = window.innerHeight;
 
-      if(rect.top < windowHeight * 0.2 && rect.bottom > 0){
-      scrollBtn.classList.add('show');
-      }
-      else{       
-        scrollBtn.classList.remove('show');
-      }
-    });
+  if (rect.top < windowHeight * 0.2 && rect.bottom > 0) {
+    scrollBtn.classList.add("show");
+  } else {
+    scrollBtn.classList.remove("show");
+  }
+});
