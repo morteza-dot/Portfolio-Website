c9daee7b71920f001a1f5e1dfbba01c72ff3a6a8
Morteza <msetoodeh13@gmail.com>
2025-11-25 00:43:25 +0330

    all set

--- contactproject/formapp/static/js/config.js
+++ contactproject/formapp/static/js/config.js
@@ -39,3 +39,20 @@ function getCookie(name) {
   }
   return cookieValue;
 }
+
+const form = document.getElementById("cform");
+
+form.addEventListener("submit", function (e) {
+  const name = document.getElementById("name").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const subject = document.getElementById("subject").value.trim();
+  const message = document.getElementById("message").value.trim();
+
+  if (!name || !email || !subject || !message) {
+    e.preventDefault();
+    alert("Please make sure all the fields have been filled out!");
+  } else {
+    alert("Form has been submitted, we'll get in touch with you ASAP");
+    location.reload();
+  }
+});
